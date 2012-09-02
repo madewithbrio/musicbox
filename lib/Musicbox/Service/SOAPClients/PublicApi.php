55f87f3a1f9c22ba903d3af288a50989e6627b61
Bruno Carreira <alfa.prime@gmail.com>
2012-09-02 01:40:56 +0100

    fix

--- lib/Musicbox/Service/SOAPClients/PublicApi.php
+++ lib/Musicbox/Service/SOAPClients/PublicApi.php
@@ -4,7 +4,7 @@ final class Musicbox_Service_SOAPClients_PublicApi extends Musicbox_Service_SOAP
 	
 	public function __construct()
 	{
-		$this->actor = "https://services.bk.sapo.pt/Music/OnDemand/PublicApi";
+		$this->actor = "https://services.sapo.pt/Music/OnDemand/PublicApi";
 		$this->wsdl = _PROJECT_LIB . '/Musicbox/Service/Contracts/Music_OnDemand_PublicApi.wsdl'; //@todo
 
 		parent::__construct();
