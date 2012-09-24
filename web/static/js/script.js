7a231831c4dd9f23bab8b53b7e9f1fff036cc4fe
Bruno Carreira <alfa.prime@gmail.com>
2012-09-24 23:21:13 +0100

    service proxy move to main javascript object

--- web/static/js/script.js
+++ web/static/js/script.js
@@ -85,7 +85,9 @@ $(document).ready(function() {
 	});
 });
 
-
+/**
+ *  Main object
+ */
 (function(scope){
 	var exportObj = {}, partialTemplates = {};
 
@@ -102,10 +104,103 @@ $(document).ready(function() {
 			var name = element.getAttribute('data-name');
 			partialTemplates[name] = element.innerHTML;
 		});
-	}
+	};
+
+	exportObj.register = function(namespace, module) {
+		var namespaces = namespace.split("."), i, b;
+		for (i = 0; i <= namespaces.length;++i) {
+			b = namespaces.slice(0, i+1).join('\'][\'');
+			if (i < namespaces.length) {
+				eval('if (typeof exportObj[\''+b+'\'] === \'undefined\') { exportObj[\''+b+'\'] = {}; }');
+			} else {
+				eval('exportObj[\''+b+'\'] = module');
+			}
+		}
+	};
 
 	$(document).ready(function(){
 		exportObj.garbagePartialTemplates();
 	});
+
 	scope.MusicBox = exportObj;
-})(window)
+})(window);
+
+/**
+ *  Service Auth proxy
+ */
+(function(runtime){
+	if (typeof window.MusicBox === 'object') {
+		window.MusicBox.register('Service.Auth', runtime);
+	} else {
+		throw "MusicBox not defined";
+	}
+})((function(){
+	var service_interface = {},
+		service_endpoint = "/auth";
+
+	var call = function(parameters, callback) {
+		var result;
+		var async = typeof callback === "function";
+		if (!async) callback = function(data){result=data;};
+
+		var xhr = $.ajax({
+			type: "POST",
+			async: async,
+			url: service_endpoint,
+			data: parameters,
+			success: callback,
+			dataType: 'json'
+		});
+		return result;
+	};
+
+	service_interface.login = function(username, password, callback) {
+		return call({username: username, password: password}, callback);
+	};
+	return service_interface;
+})());
+
+/**
+ *  Service Content proxy
+ */
+(function(runtime){
+	if (typeof window.MusicBox === 'object') {
+		window.MusicBox.register('Service.Content', runtime);
+	} else {
+		throw "MusicBox not defined";
+	}
+})((function(){
+	var service_interface = {},
+		service_endpoint = "/content";
+
+	var call = function(method, parameters, callback) {
+		var result;
+		var async = typeof callback === "function";
+		if (!async) callback = function(data){result=data;};
+
+		var xhr = $.ajax({
+			type: "POST",
+			async: async,
+			cache: false,
+			url: service_endpoint + "?method="+method,
+			data: parameters,
+			success: callback,
+			dataType: 'json'
+		});
+		
+		return result;
+	};
+
+	service_interface.GetNewAlbums = function(callback) {
+		return call('GetNewAlbums', {}, callback);
+	};
+
+	service_interface.GetAlbumById = function(AlbumId, callback) {
+		return call('GetAlbumById', {
+			AlbumId: AlbumId
+		}, callback);
+	};
+	return service_interface;
+})());
+
+
